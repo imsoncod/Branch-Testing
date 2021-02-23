ab5c7d16ff109f9e79fff4fdc15f7d2cd3457275
imsoncod <iustron@naver.com>
2021-02-23 15:47:53 +0900

    pr 수정

--- GitEx/src/main/java/com/example/demo/repository/SearchRepository.java
+++ GitEx/src/main/java/com/example/demo/repository/SearchRepository.java
@@ -1,11 +1,9 @@
 package com.example.demo.repository;
 
 import java.util.List;
-
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.demo.mapping.SearchMapping;
 import com.example.demo.vo.ViewSearchVO;
 
